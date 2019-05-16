80fb4ae2719bc7cec5a89fcdb3a29c738d2f6d24
santiagodelgado1502 <50088704+santiagodelgado1502@users.noreply.github.com>
2019-05-15 19:45:03 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea3.Clases;
+
+namespace Tarea3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new cliente("Delgado Arcentales", "Santiago Patricio", 1316895075, "La Aurora").MostrarDatos();
+            new gasolina("Extra", 5, 2.0).MostrarDatos();
+            new gasolina("Extra", 5, 2.0).HacerCalculo();
+            Console.ReadKey();
+        }
+    }
+}
